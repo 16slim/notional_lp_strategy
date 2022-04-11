fc964b34c8b6e1a61c329916fd9f0202f7e77806
16slim <16slimchance16@gmail.com>
2022-04-11 12:25:25 +0200

    feat: implemented test-clone

--- tests/test_clone.py
+++ tests/test_clone.py
@@ -1,27 +1,25 @@
+import brownie
 from utils import actions, checks, utils
 import pytest
+from brownie import reverts
 
 # tests harvesting a strategy that returns profits correctly
 def test_clone(
-    chain, accounts, token, vault, strategy, cloned_strategy, user, strategist, amount, RELATIVE_APPROX, MAX_BPS,
-    n_proxy_views, n_proxy_batch, currencyID, n_proxy_implementation, gov
+    chain, accounts, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX, MAX_BPS,
+    n_proxy_views, n_proxy_batch, currencyID, n_proxy_implementation, gov, balancer_vault, balancer_note_weth_pool
 ):
     # Deposit to the vault
-    actions.user_deposit(user, vault, token, amount);
+    actions.user_deposit(user, vault, token, amount)
 
-    # Check both strategies
-    assert strategy.currencyID() == cloned_strategy.currencyID()
-    assert strategy.address != cloned_strategy.address
-
-    # Check harvesting into the clone
-    chain.sleep(1)
-    tx = cloned_strategy.harvest({"from": gov})
-
-    account = n_proxy_views.getAccount(cloned_strategy)
-    
-    # Check wether we have entered into a position
-    assert account["portfolio"][0][3] > 0
-
-    # Sleep until maturity so the user can withdraw without reaching max_loss from the vault
-    chain.sleep(account[0][0] - chain.time() + 1)
-    chain.mine(1)
+    # Cloning a clone should not work
+    with reverts():
+        strategy.cloneStrategy(
+            vault,
+            strategist,
+            strategy.rewards(),
+            strategy.keeper(),
+            n_proxy_views.address,
+            currencyID,
+            balancer_vault.address, balancer_note_weth_pool,
+            {"from": strategist}
+        )
